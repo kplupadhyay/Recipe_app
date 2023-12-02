b41f65d3c5c841b82f7d616b27f494ec600db997
kapil <upadhyaykapil61@gmail.com>
2023-12-02 10:50:37 +0530

    Added env  and gitignore file

--- server/src/index.js
+++ server/src/index.js
@@ -3,18 +3,20 @@ import cors from "cors";
 import mongoose from "mongoose"
 import { userRouter } from "./routes/users.js";
 import { recipesRouter } from "./routes/recipes.js";
+import 'dotenv/config.js';
 
 const app = express();
-// const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL;
+
 app.use(express.json());
 app.use(cors());
 
 app.use("/auth" , userRouter);
-app.use("/recipes" , recipesRouter)
+app.use("/recipes" , recipesRouter);
 
-mongoose.connect("mongodb+srv://upadhyaykapil61:7982540594@re-cipe.k5la3vq.mongodb.net/?retryWrites=true&w=majority").then(console.log("connected to database"));
- 
+mongoose.connect(MONGO_URL).then(console.log("connected to database"));
+
 app.listen(5000,()=>{
-console.log(`serverr started`)
+console.log(`serverr started`);
 });
 
